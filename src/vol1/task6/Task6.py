7e083b33ee15d823a5d0ef22abb91c325f539fdd
KatiaShauro <katia.shauro2@gmail.com>
2025-05-09 14:55:57 +0300

    only deletion required

--- src/vol1/task6/Task6.py
+++ src/vol1/task6/Task6.py
@@ -1,22 +1,24 @@
+from copy import copy
 from typing import List, Tuple, Dict
 
 from src.vol1.tree_structure import Tree, Node
 from src.abstractions.task import Task
 
 
-class Task16_vol1(Task):
-    def __init__(self, tr: Tree):
-        self.max_way = tr.root.get_way_len()
+class Task6_vol1(Task):
+    def __init__(self):
+        self.max_way = 0
+        self.root = None
 
-    def find_max_way(self, node: Node, root: Node, stack : List):
+    def find_max_way(self, node: Node, stack : List) -> (List[Node], Node):
         u = node
         if not node:
-            return
+            return [], None
 
         way = u.get_way_len()
-        if self.max_way <= way and u.key >= root.key:
+        if self.max_way <= way and way != 2:
             stack.clear()
-            root = u
+            self.root = u
             self.max_way = way
 
         stack.append(u)
@@ -27,7 +29,7 @@ class Task16_vol1(Task):
         height_right = u.right.height if u.right else 0
         height_left = u.left.height if u.left else 0
 
-        if (len_left <= len_right or height_left <= height_right) and u.right:
+        if len_left <= len_right and height_left <= height_right and u.right:
             next_node = u.right
             root_opposit = u.left
         else:
@@ -36,24 +38,47 @@ class Task16_vol1(Task):
 
         if next_node is None:
             stack.reverse()
-            return stack
+            return stack, self.root
         else:
-            self.find_max_way(next_node, root, stack)
-            if way == self.max_way and u in stack and u.key >= root.key:
-                self.find_max_way(root_opposit, root, stack)
-        return stack
+            self.find_max_way(next_node, stack)
+            if way == self.max_way and u in stack and u.key >= self.root.key:
+                self.find_max_way(root_opposit, stack)
+        return stack, self.root
+
+
+    def first_part_of_solution(self, nodes : List[int]) -> (str, int, int):
+        s = ''
+        if not nodes:
+            return s
+        tree = Tree(nodes)
+        self.max_way = tree.root.get_way_len()
+        self.root = tree.root
+        (arr, root) = self.find_max_way(tree.root, [])
+        arr = list(reversed(arr)) if arr[-1].key < arr[0].key else arr.copy()
+        length = len(arr)
+        half = int(length/2)
+        print(length)
+        for a in arr:
+            s += str(a.key) + ' '
+
+        middle = arr[half] if length % 2 != 0 else (
+            arr[half] if (
+                    abs(root.key - arr[half].key) < abs(root.key - arr[half- 1].key)
+            ) else arr[half - 1]
+        )
+        return s, root.key, middle.key
 
 
     def execute(self):
         pass
 
+
     def get_info(self):
         pass
 
 
-#tree = Tree([50, 20, 10, 5, 30, 60, 70, 65, 64, 63, 62, 61, 80, 90, 75, 100, 95, 97])
-tree = Tree([50, 20, 10, 9, 30, 29, 28, 27, 33, 35, 34, 37, 40, 100, 120])
-#tree = Tree([60, 50, 30, 20, 40, 10, 25, 35, 33])
-t = Task16_vol1(tree)
-ar = t.find_max_way(tree.root, tree.root, [])
-print(ar)
+# tree = Tree([50, 20, 10, 5, 30, 60, 70, 65, 64, 63, 62, 61, 80, 90, 75, 100, 95, 97])
+# tree = Tree([50, 20, 10, 9, 30, 29, 28, 27, 33, 35, 34, 37, 40, 100, 120])
+# tree = Tree([60, 50, 30, 20, 40, 10, 25, 35, 33])
+#
+print(Task6_vol1().first_part_of_solution([50, 20, 10, 30, 60]))
